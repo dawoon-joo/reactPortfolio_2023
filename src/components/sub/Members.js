1ae2fde3792bb6ea498c188008ed53d53763fff9
dawoon-jo <wnekdns5015@gmail.com>
2023-01-07 19:02:32 +0900

    체크인증 함수 추가 onChange이벤트 발생시 현재 입력하고 있는 값으로 Val state변경

--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -1,6 +1,45 @@
 import Layout from '../common/Layout';
+import { useEffect, useState } from 'react';
 
 function Members() {
+	const initVal = {
+		userid: '',
+		email: '',
+		pwd1: '',
+		pwd2: '',
+	};
+	const [Val, setVal] = useState(initVal);
+	const [Err, setErr] = useState({});
+
+	const check = (value) => {
+		const errs = {};
+		const eng = /[a-zA-Z]/;
+		const num = /[0-9]/;
+		const spc = /[~!@#$%^&*()_+]/;
+		if (value.userid.length < 5) {
+			errs.userid = '아이디를 5글자 이상 입력하세요';
+		}
+		if (value.pwd1.length < 5 || !eng.test(value.pwd1) || !num.test(value.pwd1) || !spc.test(value.pwd1)) {
+			errs.pwd1 = '비밀번호는 다섯글자 이상, 영문, 숫자, 특수문자를 모두 포함하세요';
+		}
+		if (value.pwd1 !== value.pwd2) {
+			errs.pwd2 = '두개의 비밀번호를 동일하게 입력하세요';
+		}
+		if (value.email.length < 8 || !/@/.test(value.email)) {
+			errs.email = '이메일은 8글자 이상입력, @를 포함하세요.';
+		}
+		return errs;
+	};
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setVal({ ...Val, [name]: value });
+	};
+
+	useEffect(() => {
+		console.log(Val);
+	}, [Val]);
+
 	return (
 		<Layout name={'Members'}>
 			<form action=''>
@@ -14,7 +53,7 @@ function Members() {
 									<label htmlFor='userid'>USER ID</label>
 								</th>
 								<td>
-									<input type='text' name='userid' id='userid' placeholder='아이디를 입력하세요' />
+									<input type='text' name='userid' id='userid' placeholder='아이디를 입력하세요' value={Val.userid} onChange={handleChange} />
 								</td>
 							</tr>
 
@@ -24,7 +63,7 @@ function Members() {
 									<label htmlFor='pwd1'>PASSWORD</label>
 								</th>
 								<td>
-									<input type='password' name='pwd1' id='pwd1' placeholder='비밀번호를 입력하세요' />
+									<input type='password' name='pwd1' id='pwd1' placeholder='비밀번호를 입력하세요' onChange={handleChange} />
 								</td>
 							</tr>
 
@@ -34,7 +73,7 @@ function Members() {
 									<label htmlFor='pwd2'>RE PASSWORD</label>
 								</th>
 								<td>
-									<input type='password' name='pwd2' id='pwd2' placeholder='비밀번호를 재입력하세요' />
+									<input type='password' name='pwd2' id='pwd2' placeholder='비밀번호를 재입력하세요' onChange={handleChange} />
 								</td>
 							</tr>
 
@@ -44,7 +83,7 @@ function Members() {
 									<label htmlFor='email'>E-MAIL</label>
 								</th>
 								<td>
-									<input type='text' name='email' id='email' placeholder='이메일 주소를 입력하세요' />
+									<input type='text' name='email' id='email' placeholder='이메일 주소를 입력하세요' onChange={handleChange} />
 								</td>
 							</tr>
 							<tr>

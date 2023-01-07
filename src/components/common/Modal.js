4b52563e12932a7cb0dbbd55c9d10ccbc768bafb
dawoon-jo <wnekdns5015@gmail.com>
2023-01-07 15:01:31 +0900

    모달팝업 마운트시 스크롤 비활성화 모달팝업 언마운트시 스크롤 활성화

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,4 +1,12 @@
+import { useEffect } from 'react';
 function Modal(props) {
+	useEffect(() => {
+		document.body.style.overflow = 'hidden';
+
+		return () => {
+			document.body.style.overflow = 'auto';
+		};
+	}, []);
 	return (
 		<aside className='modal'>
 			<div className='con'>{props.children}</div>

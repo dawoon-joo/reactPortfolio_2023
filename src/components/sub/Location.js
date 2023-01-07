0ef92e4462d1df47c60cf78a5ddaaf9b7f9d511c
dawoon-jo <wnekdns5015@gmail.com>
2023-01-07 15:22:43 +0900

    index.html에 kakao.api링크 연결 기본지도 출력

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,9 +1,20 @@
 import Layout from '../common/Layout';
+import { useEffect, useRef } from 'react';
 
 function Location() {
+	const container = useRef(null);
+	const { kakao } = window;
+	const options = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		level: 3,
+	};
+
+	useEffect(() => {
+		const map_instance = new kakao.maps.Map(container.current, options);
+	}, []);
 	return (
 		<Layout name={'Location'}>
-			<p>Location</p>
+			<div id='map' ref={container}></div>
 		</Layout>
 	);
 }

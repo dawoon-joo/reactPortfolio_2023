88356232d9b6bfde3630f51cb2f896d4f2a599c5
dawoon-jo <wnekdns5015@gmail.com>
2023-01-07 15:36:23 +0900

    지도에 marker생성

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -9,9 +9,23 @@ function Location() {
 		level: 3,
 	};
 
+	//marker
+	const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
+	const imageSize = new kakao.maps.Size(232, 99);
+	const imageOption = { offset: new kakao.maps.Point(116, 99) };
+
+	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+	const marker = new kakao.maps.Marker({
+		position: options.center,
+		image: markerImage,
+	});
+
 	useEffect(() => {
 		const map_instance = new kakao.maps.Map(container.current, options);
+		marker.setMap(map_instance);
 	}, []);
+
 	return (
 		<Layout name={'Location'}>
 			<div id='map' ref={container}></div>

e3f7ca6e6b3a737d672c8ca97324a321dce8ebfa
dawoon-jo <wnekdns5015@gmail.com>
2023-01-08 21:25:16 +0900

    모바일 메뉴버튼 클릭시 모바일 메뉴패널 토글

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,53 +1,59 @@
 import { NavLink } from 'react-router-dom';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import Menu from './Menu';
 
 function Header(props) {
+	const menu = useRef(null);
 	const active = { color: 'aqua' };
 	return (
-		<header className={props.type}>
-			<div className='inner'>
-				<h1>
-					<NavLink exact to='/' activeStyle={active}>
-						LOGO
-					</NavLink>
-				</h1>
-				<ul id='gnb'>
-					<li>
-						<NavLink to='/department' activeStyle={active}>
-							Department
+		<>
+			<header className={props.type}>
+				<div className='inner'>
+					<h1>
+						<NavLink exact to='/' activeStyle={active}>
+							LOGO
 						</NavLink>
-					</li>
-					<li>
-						<NavLink to='/gallery' activeStyle={active}>
-							Gallery
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to='/youtube' activeStyle={active}>
-							Youtube
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to='/community' activeStyle={active}>
-							Community
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to='/location' activeStyle={active}>
-							Location
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to='/members' activeStyle={active}>
-							Membership
-						</NavLink>
-					</li>
-				</ul>
-				<FontAwesomeIcon icon={faBars} />
-			</div>
-		</header>
+					</h1>
+					<ul id='gnb'>
+						<li>
+							<NavLink to='/department' activeStyle={active}>
+								Department
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/gallery' activeStyle={active}>
+								Gallery
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/youtube' activeStyle={active}>
+								Youtube
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/community' activeStyle={active}>
+								Community
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/location' activeStyle={active}>
+								Location
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/members' activeStyle={active}>
+								Membership
+							</NavLink>
+						</li>
+					</ul>
+					<FontAwesomeIcon icon={faBars} onClick={() => menu.current.toggle()} />
+				</div>
+			</header>
+
+			<Menu ref={menu} />
+		</>
 	);
 }
 

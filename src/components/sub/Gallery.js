0eda6f482260a3f9c936ad957e6b29aa4a2493fe
dawoon-jo <wnekdns5015@gmail.com>
2023-01-08 21:16:36 +0900

    갤러리 팝업 연결

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -2,14 +2,17 @@ import Layout from '../common/Layout';
 import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import Masonry from 'react-masonry-component';
+import Modal from '../common/Modal';
 
 function Gallery() {
 	const myId = '192929270@N04';
 	const masonryOptions = { transitionDuration: '0.5s' };
 	const frame = useRef(null);
 	const input = useRef(null);
+	const modal = useRef(null);
 	const [Items, setItems] = useState([]);
 	const [Loading, setLoading] = useState(true);
+	const [Index, setIndex] = useState(0);
 
 	const getFlickr = async (opt) => {
 		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
@@ -72,44 +75,59 @@ function Gallery() {
 	}, []);
 
 	return (
-		<Layout name={'Gallery'}>
-			<div className='controls'>
-				<div className='searchBox'>
-					<input type='text' ref={input} placeholder='검색어를 입력하세요' onKeyUp={(e) => e.key === 'Enter' && showSearch()} />
-					<button onClick={showSearch}>Search</button>
-				</div>
+		<>
+			<Layout name={'Gallery'}>
+				<div className='controls'>
+					<div className='searchBox'>
+						<input type='text' ref={input} placeholder='검색어를 입력하세요' onKeyUp={(e) => e.key === 'Enter' && showSearch()} />
+						<button onClick={showSearch}>Search</button>
+					</div>
 
-				<nav>
-					<button onClick={showInterest}>Interest Gallery</button>
-					<button onClick={showMine}>My Gallery</button>
-				</nav>
-			</div>
-			{Loading && <img className='loading' src={`${process.env.PUBLIC_URL}/img/loading.gif`} alt='로딩이미지' />}
-			<div className='frame' ref={frame}>
-				<Masonry elementType='div' options={masonryOptions}>
-					{Items.map((item, idx) => {
-						return (
-							<article key={idx}>
-								<div className='inner'>
-									<div className='pic'>
-										<img src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`} alt={item.title} />
+					<nav>
+						<button onClick={showInterest}>Interest Gallery</button>
+						<button onClick={showMine}>My Gallery</button>
+					</nav>
+				</div>
+				{Loading && <img className='loading' src={`${process.env.PUBLIC_URL}/img/loading.gif`} alt='로딩이미지' />}
+				<div className='frame' ref={frame}>
+					<Masonry elementType='div' options={masonryOptions}>
+						{Items.map((item, idx) => {
+							return (
+								<article key={idx}>
+									<div className='inner'>
+										<div
+											className='pic'
+											onClick={() => {
+												setIndex(idx);
+												modal.current.open();
+											}}
+										>
+											<img src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`} alt={item.title} />
+										</div>
+										<h2>{item.title}</h2>
+										<div className='profile'>
+											<img
+												src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
+												alt={item.owner}
+												onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
+											/>
+											<span onClick={showUser}>{item.owner}</span>
+										</div>
 									</div>
-									<h2>{item.title}</h2>
-									<div className='profile'>
-										<img
-											src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
-											alt={item.owner}
-											onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
-										/>
-										<span onClick={showUser}>{item.owner}</span>
-									</div>
-								</div>
-							</article>
-						);
-					})}
-				</Masonry>
-			</div>
-		</Layout>
+								</article>
+							);
+						})}
+					</Masonry>
+				</div>
+			</Layout>
+
+			<Modal ref={modal}>
+				<img
+					src={`https://live.staticflickr.com/${Items[Index]?.server}/${Items[Index]?.id}_${Items[Index]?.secret}_b.jpg`}
+					alt={Items[Index]?.title}
+				/>
+			</Modal>
+		</>
 	);
 }
 

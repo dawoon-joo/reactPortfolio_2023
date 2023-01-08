a4ac65e23b9e11174b385a796425dd44a1290927
dawoon-jo <wnekdns5015@gmail.com>
2023-01-08 19:05:31 +0900

    메인컴포넌트에 미리보기 데이터 출력

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -3,47 +3,9 @@ import { useRef, useState, useEffect } from 'react';
 
 function Community() {
 	const getLocalData = () => {
-		const dummyPosts = [
-			{
-				title: 'The standard Lorem Ipsum passage.',
-				content:
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-			},
-			{
-				title: '1914 translation by H. Rackham',
-				content:
-					'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.',
-			},
-			{
-				title: 'It is a long established fact.',
-				content:
-					"The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
-			},
-			{
-				title: 'Contrary to popular belief.',
-				content:
-					'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form',
-			},
-			{
-				title: 'Lorem Ipsum is simply dummy text.',
-				content:
-					' It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-			},
-			{
-				title: 'The standard chunk of Lorem Ipsum used',
-				content:
-					"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. ",
-			},
-		];
 		//처음 컴포넌트 함수가 실행되자마자 로컬스토리지로부터 post키값으로 데이터를 가지고옴
 		const data = localStorage.getItem('post');
-
-		if (data) {
-			//데이터가 있으면 해당 데이터를 객체형식으로 parsing해서 리턴
-			return JSON.parse(data);
-		} else {
-			return dummyPosts;
-		}
+		return JSON.parse(data);
 	};
 
 	const input = useRef(null);
@@ -63,7 +25,7 @@ function Community() {
 			resetForm();
 			return alert('제목과 본문을 모두 입력하세요');
 		}
-		setPosts([...Posts, { title: input.current.value, content: textarea.current.value }]);
+		setPosts([{ title: input.current.value, content: textarea.current.value }, ...Posts]);
 		resetForm();
 	};
 

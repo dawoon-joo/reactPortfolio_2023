6f0424d26deb5b089932467579bd37369c9adb1f
dawoon-jo <wnekdns5015@gmail.com>
2023-01-08 20:48:13 +0900

    사용자 프로필 이미지 출력및 이미지 에러시 대체이미지 호출

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -78,6 +78,14 @@ function Gallery() {
 										<img src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`} alt={item.title} />
 									</div>
 									<h2>{item.title}</h2>
+									<div className='profile'>
+										<img
+											src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
+											alt={item.owner}
+											onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
+										/>
+										<span>{item.owner}</span>
+									</div>
 								</div>
 							</article>
 						);

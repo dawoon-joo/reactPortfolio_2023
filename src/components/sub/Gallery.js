2e211fe148bd5b30bceda369a540d66a4eb3d852
dawoon-jo <wnekdns5015@gmail.com>
2023-01-08 20:33:40 +0900

    search방식 url호출 추가

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -9,10 +9,12 @@ function Gallery() {
 		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
 		const key = 'ae5dbef0587895ed38171fcda4afb648';
 		const method_interest = 'flickr.interestingness.getList';
+		const method_search = 'flickr.photos.search';
 		const num = 20;
 		let url = '';
 
 		if (opt.type === 'interest') url = `${baseURL}&method=${method_interest}&api_key=${key}&per_page=${num}`;
+		if (opt.type === 'search') url = `${baseURL}&method=${method_search}&api_key=${key}&per_page=${num}&tags=${opt.tags}`;
 
 		const result = await axios.get(url);
 		setItems(result.data.photos.photo);

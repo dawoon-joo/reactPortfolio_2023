0016f946153e3ce095e7cc1b087c3b9aa3471a3d
dawoon-jo <wnekdns5015@gmail.com>
2023-01-08 21:29:30 +0900

    모바일 메뉴 스타일링

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -8,12 +8,73 @@ import { faSpotify } from '@fortawesome/free-brands-svg-icons';
 
 const Menu = forwardRef((props, ref) => {
 	const [Open, setOpen] = useState(false);
+	const active = { color: 'orange' };
 
 	useImperativeHandle(ref, () => {
 		return { toggle: () => setOpen(!Open) };
 	});
 
-	return <>{Open && <nav id='mobilePanel'></nav>}</>;
+	return (
+		<AnimatePresence>
+			{Open && (
+				<motion.nav
+					id='mobilePanel'
+					initial={{ opacity: 0, x: -320 }}
+					animate={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
+					exit={{ opacity: 0, x: -320, transition: { duration: 0.5 } }}
+				>
+					<h1>
+						<Link to='/'>DCODELAB</Link>
+					</h1>
+
+					<ul id='gnbMo'>
+						<li>
+							<NavLink to='/department' activeStyle={active}>
+								Department
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/community' activeStyle={active}>
+								Community
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/gallery' activeStyle={active}>
+								Gallery
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/youtube' activeStyle={active}>
+								Youtube
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/members' activeStyle={active}>
+								Member
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/location' activeStyle={active}>
+								Location
+							</NavLink>
+						</li>
+					</ul>
+
+					<ul className='brands'>
+						<li>
+							<FontAwesomeIcon icon={faYoutube} />
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faTwitter} />
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faSpotify} />
+						</li>
+					</ul>
+				</motion.nav>
+			)}
+		</AnimatePresence>
+	);
 });
 
 export default Menu;

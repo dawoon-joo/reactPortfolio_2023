5738d59eb87cd67a83f3a2152c56a01eb76ebfe5
dawoon-jo <wnekdns5015@gmail.com>
2023-01-08 21:31:41 +0900

    메뉴 클릭시 패널 닫기, 브라우저 폭이 데스크탑으로 넘어가면 패널닫기

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -1,4 +1,4 @@
-import { useState, forwardRef, useImperativeHandle } from 'react';
+import { useState, forwardRef, useImperativeHandle, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Link, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -14,6 +14,12 @@ const Menu = forwardRef((props, ref) => {
 		return { toggle: () => setOpen(!Open) };
 	});
 
+	useEffect(() => {
+		window.addEventListener('resize', () => {
+			if (window.innerWidth >= 1200) setOpen(false);
+		});
+	}, []);
+
 	return (
 		<AnimatePresence>
 			{Open && (
@@ -22,6 +28,9 @@ const Menu = forwardRef((props, ref) => {
 					initial={{ opacity: 0, x: -320 }}
 					animate={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
 					exit={{ opacity: 0, x: -320, transition: { duration: 0.5 } }}
+					onClick={() => {
+						setOpen(false);
+					}}
 				>
 					<h1>
 						<Link to='/'>DCODELAB</Link>

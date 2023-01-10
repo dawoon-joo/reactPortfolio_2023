acf961850d2a6b2071bd1d3b414c9eafe4b7c7a9
dawoon-jo <wnekdns5015@gmail.com>
2023-01-10 16:36:26 +0900

    vid 슬라이드 적용

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,4 +1,6 @@
 import { useSelector } from 'react-redux';
+import { useRef, useEffect } from 'react';
+import Anime from '../../asset/anime';
 
 function Vids({ Scrolled, currentPos }) {
 	const Vids = useSelector((store) => store.youtube.data);
@@ -7,19 +9,73 @@ function Vids({ Scrolled, currentPos }) {
 	let scroll = Scrolled - base - currentPos || 0;
 	scroll < 0 && (scroll = 0);
 
+	//slide
+	const slide = useRef(null);
+	const init = () => {
+		const panel = slide.current.children[0];
+		const lis = panel.querySelectorAll('article');
+		const len = lis.length;
+		panel.style.width = 100 * len + '%';
+		lis.forEach((el) => {
+			el.style.width = 100 / len + '%';
+		});
+		panel.prepend(panel.lastElementChild);
+	};
+	const nextSlide = () => {
+		const panel = slide.current.children[0];
+
+		new Anime(panel, {
+			prop: 'margin-left',
+			value: '-200%',
+			duration: 500,
+			callback: () => {
+				panel.append(panel.firstElementChild);
+				panel.style.marginLeft = '-100%';
+			},
+		});
+	};
+	const prevSlide = () => {
+		const panel = slide.current.children[0];
+
+		new Anime(panel, {
+			prop: 'margin-left',
+			value: '0%',
+			duration: 500,
+			callback: () => {
+				panel.prepend(panel.lastElementChild);
+				panel.style.marginLeft = '-100%';
+			},
+		});
+	};
+
+	useEffect(() => {
+		init();
+	}, []);
+
 	return (
 		<section id='vids' className='myScroll'>
-			<h1>youtube</h1>
-			<div className='youtubeWrap'>
-				{Vids.map((data, idx) => {
-					return (
-						<article key={idx}>
-							<div className='pic'>
-								<img src={data.snippet.thumbnails.high.url} alt={data.snippet.title} />
-							</div>
-						</article>
-					);
-				})}
+			<h1>
+				THRIVE IN GREAT <br /> COMPANY.EXPLORE <br />
+				CAREERS AT BAIN
+			</h1>
+			<div className='youtubeWrap' ref={slide}>
+				<div className='panel'>
+					{Vids.map((data, idx) => {
+						return (
+							<article key={idx}>
+								<div className='pic'>
+									<img src={data.snippet.thumbnails.high.url} alt={data.snippet.title} />
+								</div>
+							</article>
+						);
+					})}
+					<button className='prev' onClick={prevSlide}>
+						prev
+					</button>
+					<button className='next' onClick={nextSlide}>
+						next
+					</button>
+				</div>
 			</div>
 		</section>
 	);

b317aaf180467ecd266d0851b581a1d1cee6d046
dawoon-joo <wnekdns5015@gmail.com>
2023-01-20 22:13:30 +0900

    location 레이아웃 스타일 최종 완성(반응형 포함)

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -12,25 +12,34 @@ function Location() {
 
 	info.current = [
 		{
-			title: '넥슨 본사',
+			title: 'DIRECT CONTACT',
+			subtitle: 'head shop',
+			description:
+				'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos ipsam ab reiciendis maiores iure omnis autem sapiente numquam architecto tempore.',
 			latlng: new kakao.maps.LatLng(37.40211707077346, 127.10344953763003),
-			imgUrl: `${process.env.PUBLIC_URL}/img/marker1.png`,
-			imgSize: new kakao.maps.Size(232, 99),
-			imgPos: { offset: new kakao.maps.Point(116, 99) },
+			imgUrl: `${process.env.PUBLIC_URL}/img/marker.png`,
+			imgSize: new kakao.maps.Size(80, 80),
+			imgPos: { offset: new kakao.maps.Point(40, 80) },
 		},
 		{
-			title: '올림픽 공원',
+			title: 'MEDIA CONTACT',
+			subtitle: 'branch',
+			description:
+				'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos ipsam ab reiciendis maiores iure omnis autem sapiente numquam architecto tempore.',
 			latlng: new kakao.maps.LatLng(37.5206868, 127.1214941),
-			imgUrl: `${process.env.PUBLIC_URL}/img/marker2.png`,
-			imgSize: new kakao.maps.Size(232, 99),
-			imgPos: { offset: new kakao.maps.Point(116, 99) },
+			imgUrl: `${process.env.PUBLIC_URL}/img/marker.png`,
+			imgSize: new kakao.maps.Size(80, 80),
+			imgPos: { offset: new kakao.maps.Point(40, 80) },
 		},
 		{
-			title: '서울 시청',
+			title: 'BETTER TOCENTHER',
+			subtitle: 'branch',
+			description:
+				'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos ipsam ab reiciendis maiores iure omnis autem sapiente numquam architecto tempore.',
 			latlng: new kakao.maps.LatLng(37.5662952, 126.9779451),
-			imgUrl: `${process.env.PUBLIC_URL}/img/marker3.png`,
-			imgSize: new kakao.maps.Size(232, 99),
-			imgPos: { offset: new kakao.maps.Point(116, 99) },
+			imgUrl: `${process.env.PUBLIC_URL}/img/marker.png`,
+			imgSize: new kakao.maps.Size(80, 80),
+			imgPos: { offset: new kakao.maps.Point(40, 80) },
 		},
 	];
 
@@ -85,29 +94,44 @@ function Location() {
 
 	return (
 		<Layout name={'Location'}>
-			<div id='map' ref={container}></div>
-			<nav>
-				<button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic ON' : 'Traffic OFF'}</button>
-				<ul className='branch'>
-					{info.current.map((el, idx) => {
-						let isOn = '';
-						Index === idx && (isOn = 'on');
-						return (
-							<li
-								key={idx}
-								className={isOn}
-								onClick={() => {
-									//각 버튼 클릭시 클릭한 순번으로 Index state변경
-									setIndex(idx);
-									setTraffic(false);
-								}}
-							>
-								{el.title}
-							</li>
-						);
-					})}
-				</ul>
-			</nav>
+			<div className='inner'>
+				<div className='mapBoxs'>
+					<div className='txtBoxs'>
+						<h3>CONTACT</h3>
+						<h2>GET IN TOUCH</h2>
+						<span>Lorem ipsum dolor sit amet.</span>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos ipsam ab reiciendis maiores iure omnis autem sapiente numquam architecto
+							tempore.
+						</p>
+					</div>
+					<div id='map' ref={container}></div>
+				</div>
+				<nav>
+					{/* <button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic ON' : 'Traffic OFF'}</button> */}
+					<ul className='branch'>
+						{info.current.map((el, idx) => {
+							let isOn = '';
+							Index === idx && (isOn = 'on');
+							return (
+								<li
+									key={idx}
+									className={isOn}
+									onClick={() => {
+										//각 버튼 클릭시 클릭한 순번으로 Index state변경
+										setIndex(idx);
+										setTraffic(false);
+									}}
+								>
+									<h2>{el.title}</h2>
+									<h3>{el.subtitle}</h3>
+									<p>{el.description}</p>
+								</li>
+							);
+						})}
+					</ul>
+				</nav>
+			</div>
 		</Layout>
 	);
 }

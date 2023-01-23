76c28c49abf56420f5d5341e99f2759d9f5e8762
dawoon-joo <wnekdns5015@gmail.com>
2023-01-24 00:10:00 +0900

    members페이지 최종 작업 완료

--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -1,6 +1,8 @@
 import Layout from '../common/Layout';
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function Members() {
 	const history = useHistory();
@@ -65,84 +67,117 @@ function Members() {
 
 	return (
 		<Layout name={'Members'}>
-			<form action='' onSubmit={handleSubmit}>
-				<fieldset>
-					<legend className='h'>회원가입 폼 양식</legend>
-					<table border='1'>
-						<tbody>
-							{/* user id */}
-							<tr>
-								<th scope='row'>
-									<label htmlFor='userid'>USER ID</label>
-								</th>
-								<td>
-									<input type='text' name='userid' id='userid' placeholder='아이디를 입력하세요' value={Val.userid} onChange={handleChange} />
-									<span className='err'>{Err.userid}</span>
-								</td>
-							</tr>
+			<div className='boxWrap'>
+				<form action='' onSubmit={handleSubmit}>
+					<fieldset>
+						<legend className='h'>회원가입 폼 양식</legend>
+						<table border='1'>
+							<tbody>
+								{/* user id */}
+								<tr>
+									<th scope='row'>
+										<label htmlFor='userid'>USER ID</label>
+									</th>
+									<td>
+										<input type='text' name='userid' id='userid' placeholder='아이디를 입력하세요' value={Val.userid} onChange={handleChange} />
+										<span className='err'>{Err.userid}</span>
+									</td>
+								</tr>
 
-							{/* password */}
-							<tr>
-								<th scope='row'>
-									<label htmlFor='pwd1'>PASSWORD</label>
-								</th>
-								<td>
-									<input type='password' name='pwd1' id='pwd1' placeholder='비밀번호를 입력하세요' value={Val.pwd1} onChange={handleChange} />
-									<span className='err'>{Err.pwd1}</span>
-								</td>
-							</tr>
+								{/* password */}
+								<tr>
+									<th scope='row'>
+										<label htmlFor='pwd1'>PASSWORD</label>
+									</th>
+									<td>
+										<input type='password' name='pwd1' id='pwd1' placeholder='비밀번호를 입력하세요' value={Val.pwd1} onChange={handleChange} />
+										<span className='err'>{Err.pwd1}</span>
+									</td>
+								</tr>
 
-							{/* re password */}
-							<tr>
-								<th scrope='row'>
-									<label htmlFor='pwd2'>RE PASSWORD</label>
-								</th>
-								<td>
-									<input type='password' name='pwd2' id='pwd2' placeholder='비밀번호를 재입력하세요' value={Val.pwd2} onChange={handleChange} />
-									<span className='err'>{Err.pwd2}</span>
-								</td>
-							</tr>
+								{/* re password */}
+								<tr>
+									<th scrope='row'>
+										<label htmlFor='pwd2'>RE PASSWORD</label>
+									</th>
+									<td>
+										<input type='password' name='pwd2' id='pwd2' placeholder='비밀번호를 재입력하세요' value={Val.pwd2} onChange={handleChange} />
+										<span className='err'>{Err.pwd2}</span>
+									</td>
+								</tr>
 
-							{/* email */}
-							<tr>
-								<th scope='row'>
-									<label htmlFor='email'>E-MAIL</label>
-								</th>
-								<td>
-									<input type='text' name='email' id='email' placeholder='이메일 주소를 입력하세요' value={Val.email} onChange={handleChange} />
-									<span className='err'>{Err.email}</span>
-								</td>
-							</tr>
+								{/* email */}
+								<tr>
+									<th scope='row'>
+										<label htmlFor='email'>E-MAIL</label>
+									</th>
+									<td>
+										<input type='text' name='email' id='email' placeholder='이메일 주소를 입력하세요' value={Val.email} onChange={handleChange} />
+										<span className='err'>{Err.email}</span>
+									</td>
+								</tr>
 
-							{/* comments */}
-							<tr>
-								<th scope='row'>
-									<label htmlFor='comments'>COMMENTS</label>
-								</th>
-								<td>
-									<textarea
-										name='comments'
-										id='comments'
-										cols='30'
-										rows='5'
-										value={Val.comments}
-										onChange={handleChange}
-										placeholder='남기는 말을 입력하세요.'
-									></textarea>
-									<span className='err'>{Err.comments}</span>
-								</td>
-							</tr>
+								{/* comments */}
+								<tr>
+									<th scope='row'>
+										<label htmlFor='comments'>COMMENTS</label>
+									</th>
+									<td>
+										<textarea
+											name='comments'
+											id='comments'
+											cols='30'
+											rows='5'
+											value={Val.comments}
+											onChange={handleChange}
+											placeholder='남기는 말을 입력하세요.'
+										></textarea>
+										<span className='err'>{Err.comments}</span>
+									</td>
+								</tr>
 
-							<tr>
-								<th colSpan='2'>
-									<input type='reset' value='cancel' onClick={handleReset} />
-									<input type='submit' value='send' />
-								</th>
-							</tr>
-						</tbody>
-					</table>
-				</fieldset>
-			</form>
+								<tr>
+									<th colSpan='2'>
+										<input type='reset' value='Cancel' onClick={handleReset} />
+										<input type='submit' value='Send' />
+									</th>
+								</tr>
+							</tbody>
+						</table>
+					</fieldset>
+				</form>
+				<div className='txtBox'>
+					<h2>Lorem, ipsum dolor.</h2>
+					<p>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, libero sapiente earum inventore nam quia praesentium doloribus nemo
+						aspernatur quibusdam suscipit quidem. Facilis quia consequuntur reiciendis veniam recusandae modi cum.
+					</p>
+					<span>Lorem, ipsum dolor.</span>
+					<span>Lorem, ipsum dolor.</span>
+					<span>Lorem, ipsum dolor.</span>
+				</div>
+			</div>
+			<div className='txtWrap'>
+				<article>
+					<h2>Lorem, ipsum dolor.</h2>
+					<p>E: wnekdns5015@gmail.com</p>
+					<p>P: 010-2638-3054</p>
+					<FontAwesomeIcon icon={faTwitter} />
+					<FontAwesomeIcon icon={faFacebook} />
+					<FontAwesomeIcon icon={faInstagram} />
+				</article>
+				<article>
+					<h2>Lorem, ipsum dolor.</h2>
+					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, enim!</p>
+				</article>
+				<article>
+					<h2>Lorem, ipsum dolor.</h2>
+					<h3>Lorem ipsum dolor sit amet.</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur tempore sit ea expedita iste maxime minima! Eaque reiciendis quia quasi.
+					</p>
+				</article>
+			</div>
 		</Layout>
 	);
 }
